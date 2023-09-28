6f265ab92699acab9a5da1da4a25b1c7f9351728
Adam Tak <adam.tak2112@gmail.com>
2023-09-28 10:52:12 -0600

    add main menu logic

--- update/update.sh
+++ update/update.sh
@@ -14,39 +14,59 @@ adbConnect() {
 adb kill-server
 adb start-server
 
-# Start menu for what device to update
+#Menu for what device to update
 selectDevice(){
-options=("22\" Bike" "10\" Rower" "10\" Treadmill" "Exit")
-echo "Select which device you would like to update:"
+    options=("22\" Bike" "10\" Rower" "10\" Treadmill" "Exit")
+    echo "Select which device you would like to update:"
 
-select opt in "${options[@]}"
-do
-    case $opt in 
-        "22\" Bike")
-            echo "Connecting your 22\" bike..."
-            adbConnect '192.168.0.35'
+    select opt in "${options[@]}"
+        do
+            case $opt in 
+            "22\" Bike")
+                echo "Connecting your 22\" bike..."
+                adbConnect '192.168.0.35'
             
-            ;;
-        "10\" Rower")
-            adbConnect '192.168.0.41'
+                ;;
+            "10\" Rower")
+                adbConnect '192.168.0.41'
             
-            ;;
-        "10\" Treadmill")
-            adbConnect '192.168.0.40'
+                ;;
+            "10\" Treadmill")
+                adbConnect '192.168.0.40'
             
-            ;;
-        "Exit")
-            echo "Exiting the script..."
-            break;
-            ;;
-        *)
-            echo "Invalid option"
-    esac
-done
-
+                ;;
+            "Exit")
+                echo "Exiting connection attempt..."
+                break
+                ;;
+            *)
+                echo "Invalid option"
+            esac
+        done
 }
 
 ###########################################
 
 #main menu --
 ### options: connect devices, update devices, exit
+startOptions=("Connect Devices" "Update Devices" "Exit")
+
+echo "What would you like to do?"
+
+select choice in "${startOptions[@]}"
+do
+    case $choice in
+        "Connect Devices")
+            echo "Reached connect devices"
+            ;;
+        "Update Devices")
+            echo "Reached update devices"
+            ;;
+        "Exit")
+            echo "Exiting Script, have a nice day!"
+            break;
+            ;;
+        *)
+            echo "Invalid"
+    esac
+done
